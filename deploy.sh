d6b6c53970f9e197ba7d0bbfc902184d76cdd279
chubi-x <chubixax@protonmail.com>
2025-08-23 18:46:18 +0100

    update deploy script
    update deploy script
    
    update deploy script
    
    update deploy script
    
    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ set -e
 USER=ubuntu
 DIR=/home/ubuntu/spotiplay
 SERVICE=spotiplay
-VENV="$DIR/venv"
+VENV="$DIR/.venv"
 SYSTEMD_SERVICE_FILE=spotiplay.service
 NGINX_CONF_FILE=spotiplay_nginx.conf
 SYSTEMD_DEST=/etc/systemd/system/$SYSTEMD_SERVICE_FILE
@@ -15,24 +15,24 @@ NGINX_DEST=/etc/nginx/sites-available/spotiplay
 NGINX_SYMLINK=/etc/nginx/sites-enabled/spotiplay
 
 cd "$DIR"
+source $HOME/.local/bin/env
 echo "[deploy.sh] Pulling latest changes..."
 git pull --ff-only
 
 if [ ! -d "$VENV" ]; then
-    echo "[deploy.sh] Creating venv..."
-    python3 -m venv "$VENV"
+	echo "[deploy.sh] Creating venv..."
+	curl -LsSf https://astral.sh/uv/install.sh | sh
+	uv venv
 fi
 
 source "$VENV/bin/activate"
 
 if [ -f requirements-prod.txt ]; then
-    echo "[deploy.sh] Installing prod requirements..."
-    pip install --upgrade pip
-    pip install -r requirements-prod.txt
+	echo "[deploy.sh] Installing prod requirements..."
+	uv pip install -r requirements-prod.txt
 else
-    echo "[deploy.sh] Installing requirements.txt..."
-    pip install --upgrade pip
-    pip install -r requirements.txt
+	echo "[deploy.sh] Installing requirements.txt..."
+	uv pip install -r requirements.txt
 fi
 
 echo "[deploy.sh] Installing systemd service file..."
@@ -42,11 +42,10 @@ sudo systemctl daemon-reload
 echo "[deploy.sh] Installing nginx config..."
 sudo cp "$DIR/$NGINX_CONF_FILE" "$NGINX_DEST"
 if [ ! -e "$NGINX_SYMLINK" ]; then
-    sudo ln -s "$NGINX_DEST" "$NGINX_SYMLINK"
+	sudo ln -s "$NGINX_DEST" "$NGINX_SYMLINK"
 fi
 sudo nginx -t && sudo systemctl reload nginx
 
 echo "[deploy.sh] Restarting systemd service..."
 sudo systemctl restart "$SERVICE"
 echo "[deploy.sh] Deployment complete."
-
